c987185604e6f31343d295c70f29038172d5a6ba
Morty <samwu0808@outlook.com>
2021-10-25 15:17:05 -0500

    Update and rename ftps_helper.py to ftps_client.py

---
+++ data_access/ftps_client.py
@@ -0,0 +1,23 @@
+import argparse
+import logging
+import datetime
+from paramiko import SFTPClient, Transport
+from contextlib import contextmanager
+
+
+class FTPClient:
+    def __init__(self, host, password, username, port=22):
+        self.host = host
+        self.port = port
+        self.password = password
+        self.username = username
+
+    @contextmanager
+    def get_conn(self):
+        transport = Transport(sock=(self.host, self.port))
+        transport.connect(username=self.username, password=self.password)
+        conn = SFTPClient.from_transport(transport)
+        try:
+            yield conn
+        finally:
+            conn.close()
