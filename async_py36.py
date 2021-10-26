0c8d000ef82aaf2b1bfb98f579c8e92e7c8ca183
Morty <samwu0808@outlook.com>
2021-10-26 11:15:05 -0500

    Update async_py36.py

--- async_py36.py
+++ async_py36.py
@@ -7,6 +7,7 @@ def async_run(coro):
     @functools.wraps(coro)
     def run_with_event_loop(*args, **kwargs):
         loop = asyncio.get_event_loop()
-        return loop.run_until_complete(coro(*args, **kwargs))
-
+        res =  loop.run_until_complete(coro(*args, **kwargs))
+        return res
+    
     return run_with_event_loop
