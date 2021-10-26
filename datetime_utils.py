baaf3133b8b802844c58c4be6676c443391bbf0c
Morty <samwu0808@outlook.com>
2021-10-26 16:20:06 -0500

    Update datetime_utils.py

--- datetime_utils.py
+++ datetime_utils.py
@@ -7,3 +7,9 @@ def to_start_on_date(d: datetime.date) -> datetime.datetime:
 
 def to_end_on_date(d: datetime.date) -> datetime.datetime:
     return to_start_on_date(d) + datetime.timedelta(seconds=86400 - 1)
+
+def convert_datetime_to_tz(d: datetime.datetime, tzinfo) -> datetime.datetime:
+    return d.astimezone(tzinfo)
+
+def attach_tzinfo_to_datetime(d: datetime.datetime, tzinfo) -> datetime.datetime:
+    return tzinfo.localize(d)
