f96cf182337285a21bab2f0668545bc122f2d20b
Morty <samwu0808@outlook.com>
2022-02-08 13:00:57 -0600

    Update write_csv_to_sf.py

--- write_csv_to_sf.py
+++ write_csv_to_sf.py
@@ -1,6 +1,7 @@
 import itertools
 import threading
 import pandas as pd
+import snowflake
 
 class LockedIterator:
     def __init__(self, it):
@@ -14,8 +15,9 @@ class LockedIterator:
         with self._lock:
             return next(self._it)
 
-flatten_iter = itertools.chain.from_iterable
+
 def get_factors(n):
+    flatten_iter = itertools.chain.from_iterable
     return sorted(list((flatten_iter((i, n//i) 
                 for i in range(1, int(n**0.5)+1) if n % i == 0))))
 
@@ -31,42 +33,41 @@ def get_row_cnt_for_csv(filename):
             cnt = sum(1 for _ in f)
             return cnt
         
-row_cnt = get_row_cnt_for_csv("test.csv.gz")
-
-
-factors = get_factors(row_cnt)
-
-for factor in factors[::-1]:
-    if factor < 1_000_000:
-        chunksize = factor
-        break
+def get_chunksize(factors):
+    for factor in factors[::-1]:
+        if factor < 1_000_000:
+            chunksize = factor
+            return chunksize
         
-
-creds = {"user": "user",
-         "password": "pwd",
-         "account":'east-us-2.azure',
-         "warehouse": "wh",
-         "database": "db", 
-         "schema": "schema"}
-
 def get_sf_conn(creds):
     conn = snowflake.connector.connect(**creds)
     return conn
 
-data = LockedIterator(pd.read_csv("test.csv.gz", chunksize=chunksize))
-
-def read():
-    sf_conn = get_sf_conn(creds)
-    while 1:
-        global data
-        try:
-            tmp = next(data)
-            write_pandas(sf_conn, tmp, "your_table", "your_db", "your_schema")
-        except:
-            break
+def upload_csv_to_sf(filename, table, db, schema):
+    creds = {"user": "user",
+         "password": "password",
+         "account":'account',
+         "warehouse": "warehouse",
+         "database": "database", 
+         "schema": "schema"}
     
-threads =[threading.Thread(target=read) for i in range(20)]
-for t in threads:
-    t.start()
-for t in threads:
-    t.join()
+    row_cnt = get_row_cnt_for_csv(filename)
+    factors = get_factors(row_cnt)
+    chunksize = get_chunksize(factors)
+    
+    data = LockedIterator(pd.read_csv(filename, chunksize=chunksize))
+    
+    def _upload():
+        sf_conn = get_sf_conn(creds)
+        while 1:
+            try:
+                tmp = next(data)
+                write_pandas(sf_conn, tmp, table, db, schema)
+            except:
+                break
+                
+    threads =[threading.Thread(target=_upload) for i in range(20)]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
