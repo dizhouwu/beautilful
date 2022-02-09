b17d9d76d752cd35fc71552180cf7939549e30d8
Morty <samwu0808@outlook.com>
2022-02-09 10:01:26 -0600

    Update io_utils.py

--- io_utils.py
+++ io_utils.py
@@ -5,3 +5,6 @@ import gzip
 io_buffer = io.BytesIO()
 io_buffer.seek(0)
 compressed_bytes = gzip.compress(io_buffer.read())
+
+with gzip.open(gz_file_path, mode="wb") as f_out:
+    shutil.copyfileobj(f, f_out)  # copyfileobj will perform chunking by default
